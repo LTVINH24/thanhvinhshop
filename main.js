285d6d19054080413513cbe20869f301e438511f
Le Thanh Vinh <thanhvinh2400@gmail.com>
2024-07-16 11:32:13 +0700

    demo1

---
+++ main.js
@@ -0,0 +1,24 @@
+const modal1=document.querySelector('.modal1')
+const resit1=document.querySelector(".click1")
+const back1=document.querySelector('.return1')
+resit1.onclick=function()
+{
+    
+    modal1.style.display='flex';
+}
+back1.onclick=function() 
+{
+    modal1.style.display='none';
+}
+const modal2=document.querySelector('.modal2')
+const resit2=document.querySelector(".click2")
+const back2=document.querySelector('.return2')
+resit2.onclick=function()
+{
+    
+    modal2.style.display='flex';
+}
+back2.onclick=function() 
+{
+    modal2.style.display='none';
+}
